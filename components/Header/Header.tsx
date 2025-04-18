0dc4c988b4aefe2b78dc6262da86c8af9eb0c27c
murphyslaw-lytics <iain.murphy@lytics.com>
2025-04-18 09:08:44 +0100

    Update Header.tsx to include Lytics library

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -124,8 +124,27 @@ function Header (props: App.Header): JSX.Element {
         if (mobile) resetMobileNav()
     }
 
+    // Allows javascript library to be called into page
+    import Script from 'next/script';
 
     return (
+        <Script    
+            <!-- Start Lytics Tracking Tag Version 3 -->
+              <script type="text/javascript">
+                  !function(){"use strict";var o=window.jstag||(window.jstag={}),r=[];function n(e){o[e]=function(){for(var n=arguments.length,t=new Array(n),i=0;i<n;i++)t[i]=arguments[i];r.push([e,t])}}n("send"),n("mock"),n("identify"),n("pageView"),n("unblock"),n("getid"),n("setid"),n("loadEntity"),n("getEntity"),n("on"),n("once"),n("call"),o.loadScript=function(n,t,i){var e=document.createElement("script");e.async=!0,e.src=n,e.onload=t,e.onerror=i;var o=document.getElementsByTagName("script")[0],r=o&&o.parentNode||document.head||document.body,c=o||r.lastChild;return null!=c?r.insertBefore(e,c):r.appendChild(e),this},o.init=function n(t){return this.config=t,this.loadScript(t.src,function(){if(o.init===n)throw new Error("Load error!");o.init(o.config),function(){for(var n=0;n<r.length;n++){var t=r[n][0],i=r[n][1];o[t].apply(o,i)}r=void 0}()}),this}}();
+              // Define config and initialize Lytics tracking tag.
+              // - The setup below will disable the automatic sending of Page Analysis Information (to prevent duplicative sends, as this same information will be included in the jstag.pageView() call below, by default)
+                  jstag.init({
+                    src: 'https://c.lytics.io/api/tag/ace594a431d70e90c5882862b94da308/latest.min.js'
+                            });
+  
+              // You may need to send a page view, depending on your use-case
+                  jstag.pageView();
+        
+        strategy="beforeInteractive" // Ensures the script loads in the <head> before React runs
+        onLoad={() => {
+        console.log('Library loaded!');}}
+/>
         <header id='header-component' className={`${isHome ? 'mt-[-80px]' : ''} bg-white sticky top-0 z-50 hover:bg-white ${(!Opac) ? 'bg-opacity-100' : 'bg-opacity-20'}`}>
             <nav className={`mx-auto flex items-center justify-between px-4 py-5 lg:px-8 ${mobileMenuOpen ? 'hidden sm:block' : ''}`} aria-label='Global'>
                 <div className='flex lg:flex-1`'>
